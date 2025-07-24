ea5cafd7885eb2e5c62353e4b86d33eb77f01916
Lucas Franco <127855437+lucasfc-dev@users.noreply.github.com>
2025-07-24 13:19:45 -0300

    Adicionando titulo

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "NEGOLAS SYSTEM HAHA",
+  title: "Gerenciador de Arquivos",
   description: "BY LEVI, PIRANHA",
 };
 
